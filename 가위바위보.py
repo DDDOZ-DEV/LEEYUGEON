0c5c6401809de3f3a19ae5da13a99ce5bc103ae7
LEE YU GEON <eeeee22222iy@gmail.com>
2026-04-21 21:38:04 +0900

    Create 가위바위보.py

---
+++ 가위바위보.py
@@ -0,0 +1,17 @@
+SCISSOR = '가위'
+ROCK = '바위'
+PAPER = '보'
+
+WIN = '이겼다'
+DRAW = '비겼다'
+LOSE = '졌다'
+
+mine = '가위'
+yours = '바위'
+
+if mime == yours:
+  result = DRAW
+else:
+  result = '이기거나 지거나'
+
+print(result)
